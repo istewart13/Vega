c1a57c5d93d62b1434540139624de14cb9d10a04
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-11-19 15:54:00 +0000

    Add /Makes route to controller

---
+++ Vega/Controllers/MakesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vega.Controllers.Resources;
+using Vega.Models;
+using Vega.Persistence;
+
+namespace Vega.Controllers
+{
+    [Produces("application/json")]
+    public class MakesController : Controller
+    {
+        private readonly VegaDbContext _context;
+        private readonly IMapper _mapper;
+
+        public MakesController(VegaDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet("/api/Makes")]
+        public async Task<IEnumerable<MakeResource>> GetMakes()
+        {
+            var makes = await _context.Makes.Include(m => m.Models).ToListAsync();
+            return _mapper.Map<List<Make>, List<MakeResource>>(makes);
+        }
+    }
+}
