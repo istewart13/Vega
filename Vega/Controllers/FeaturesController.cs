392366d94b538e97318d29642ec4502bf5b7226d
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-11-20 14:28:09 +0000

    Add Features controller w/ GetFeatures action

---
+++ Vega/Controllers/FeaturesController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vega.Controllers.Resources;
+using Vega.Models;
+using Vega.Persistence;
+
+namespace Vega.Controllers
+{
+    public class FeaturesController : Controller
+    {
+        private readonly VegaDbContext _context;
+        private readonly IMapper _mapper;
+
+        public FeaturesController(VegaDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet("/api/Features")]
+        public async Task<IEnumerable<FeatureResource>> GetFeatures()
+        {
+            var features = await _context.Features.ToListAsync();
+            return _mapper.Map<List<Feature>, List<FeatureResource>>(features);
+        }
+    }
+}
