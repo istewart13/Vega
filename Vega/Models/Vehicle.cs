a43163fb04ae20d244ab078fb5f40475f84c7394
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-11-27 16:22:03 +0000

    Create many-to-many relationship between Vehicle and Feature

--- Vega/Models/Vehicle.cs
+++ Vega/Models/Vehicle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,9 +12,24 @@ namespace Vega.Models
         public int ModelId { get; set; }
         public Model Model { get; set; }
         public bool IsRegistered { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string ContactName { get; set; }
+
+        [StringLength(255)]
         public string ContactEmail { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string ContactPhone { get; set; }
         public DateTime LastUpdate { get; set; }
+
+        public ICollection<VehicleFeature> Features { get; set; }
+
+        public Vehicle()
+        {
+            Features = new CollectionExtensions<VehicleFeature>();
+        }
     }
 }
