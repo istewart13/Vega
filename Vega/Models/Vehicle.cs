078ac703fb59782178351cbe013975acbdb30997
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-11-27 15:59:09 +0000

    Add Vehicle model class

---
+++ Vega/Models/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vega.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public int ModelId { get; set; }
+        public Model Model { get; set; }
+        public bool IsRegistered { get; set; }
+        public string ContactName { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactPhone { get; set; }
+        public DateTime LastUpdate { get; set; }
+    }
+}
