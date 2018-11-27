de481f9e82aab61d78c95c725682395cc06f9fab
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-11-27 16:33:44 +0000

    Add table name to Vehicle and VehicleFeatures

--- Vega/Models/Vehicle.cs
+++ Vega/Models/Vehicle.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Vega.Models
 {
+    [Table("Vehicles")]
     public class Vehicle
     {
         public int Id { get; set; }
@@ -29,7 +31,7 @@ namespace Vega.Models
 
         public Vehicle()
         {
-            Features = new CollectionExtensions<VehicleFeature>();
+            Features = new Collection<VehicleFeature>();
         }
     }
 }
--- Vega/Models/VehicleFeature.cs
+++ Vega/Models/VehicleFeature.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Vega.Models
 {
+    [Table("VehicleFeatures")]
     public class VehicleFeature
     {
         public int VehicleId { get; set; }
