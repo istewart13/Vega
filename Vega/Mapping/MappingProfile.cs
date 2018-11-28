b9432714749bc102d49437dbc94fc1a19ce35f12
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-11-28 16:02:35 +0000

    Add mapping for VehicleResource => Vehicle

--- Vega/Mapping/MappingProfile.cs
+++ Vega/Mapping/MappingProfile.cs
@@ -12,9 +12,19 @@ namespace Vega.Mapping
     {
         public MappingProfile()
         {
+            // Domain to API resource
             CreateMap<Make, MakeResource>();
             CreateMap<Model, ModelResource>();
             CreateMap<Feature, FeatureResource>();
+
+            // API resource to Domain
+            CreateMap<VehicleResource, Vehicle>()
+                .ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))
+                .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
+                .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
+                .ForMember(v => v.Features, opt => opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature { FeatureId = id })));
+
+
         }
     }
 }
