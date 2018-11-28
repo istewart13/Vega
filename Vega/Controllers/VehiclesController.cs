d2b84a0484a82b9929f83d5bfa54f096210f9f17
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-11-28 15:20:56 +0000

    Add CreateVehicle action to VehiclesController

---
+++ Vega/Controllers/VehiclesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Vega.Models;
+
+namespace Vega.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/vehicles")]
+    public class VehiclesController : Controller
+    {
+        [HttpPost]
+        public IActionResult CreateVehicle([FromBody] Vehicle vehicle)
+        {
+            return Ok(vehicle);
+        }
+    }
+}
